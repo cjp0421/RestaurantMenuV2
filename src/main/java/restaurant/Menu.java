0180e931c53037d0cb5ca26a623174dc371c17f0
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-04-04 20:11:30 -0500

    restaurant menu menu class finished

--- src/main/java/restaurant/Menu.java
+++ src/main/java/restaurant/Menu.java
@@ -1,9 +1,11 @@
 package restaurant;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
-    public static void main(String[] args) {
-
-    }
+    //public static void main(String[] args) {
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private Date lastUpdated;
+    //}
 }
