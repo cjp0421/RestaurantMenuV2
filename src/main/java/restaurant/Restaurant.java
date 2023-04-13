ac7ec5bca59122344be42240dc58b55820f71d41
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-04-12 21:20:55 -0500

    restaurant menu item - completed the rest of the studio todos by testing the functionality

--- src/main/java/restaurant/Restaurant.java
+++ src/main/java/restaurant/Restaurant.java
@@ -20,7 +20,19 @@ public class Restaurant {
         menu1.addMenuItem(item4);
         menu1.addMenuItem(item5);
 
+        // Print 1 item
 //        System.out.println(item2.toString());
+        //prints menu
         menu1.getMenuItems();
-    }
-}
+        //remove and reprint the menu
+        menu1.removeItem(item5);
+        menu1.getMenuItems();
+
+        //test equals method
+        System.out.println(item1.equals(item2));
+
+
+    //try to add a duplicate item
+    MenuItem item6 = new MenuItem("Fried Cheese", "Cheese - breaded and fried", 5.00, "Appetizer");
+    menu1.addMenuItem(item6);
+}}
